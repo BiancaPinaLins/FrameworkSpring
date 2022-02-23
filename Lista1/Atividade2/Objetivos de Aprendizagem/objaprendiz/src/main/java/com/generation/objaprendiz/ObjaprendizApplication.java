74768d26ace26a7f23b4666290f4cb9c812cee75
BiancaPinaLins <bianca_palins@outlook.com>
2022-02-23 13:13:20 -0300

    Spring - Atividade 2

---
+++ Lista1/Atividade2/Objetivos de Aprendizagem/objaprendiz/src/main/java/com/generation/objaprendiz/ObjaprendizApplication.java
@@ -0,0 +1,13 @@
+package com.generation.objaprendiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjaprendizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjaprendizApplication.class, args);
+	}
+
+}
---
+++ Lista1/Atividade2/Objetivos de Aprendizagem/objaprendiz/src/test/java/com/generation/objaprendiz/ObjaprendizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.objaprendiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjaprendizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
